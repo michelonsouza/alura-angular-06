11c4ae6bbb75ace0659fa819b7a5067f2123102f
Michelon Souza <michelon.souza@hotmail.com>
2024-02-05 11:27:47 -0300

    initial commit

---
+++ src/app/animations.ts
@@ -0,0 +1,53 @@
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
+
+export const highlightedStateTrigger = trigger('highlightedState', [
+  state(
+    'default',
+    style({
+      border: '2px solid #b2b6ff',
+    })
+  ),
+  state(
+    'highlighted',
+    style({
+      border: '4px solid #b2b6ff',
+      filter: 'brightness(0.92)',
+    })
+  ),
+  transition('default => highlighted', [
+    animate(
+      '200ms ease-out',
+      style({
+        transform: 'scale(1.02)',
+      })
+    ),
+    animate(200),
+  ]),
+]);
+
+export const shownStateTrigger = trigger('shownState', [
+  transition(':enter', [
+    style({
+      opacity: 0,
+    }),
+    animate(300, style({ opacity: 1 })),
+  ]),
+  transition(':leave', [animate(300, style({ opacity: 0 }))]),
+]);
+
+export const checkButtonTrigger = trigger('checkButton', [
+  transition('* => checked', [
+    animate(
+      '400ms ease-in',
+      style({
+        transform: 'scale(0.4)',
+      })
+    ),
+  ]),
+]);
